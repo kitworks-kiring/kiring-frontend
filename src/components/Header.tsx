3a82acf11e80317fe0db00ddda54c3411b303d70
hyein0112 <s21046@gsm.hs.kr>
2025-05-20 18:26:34 +0900

    feat: Header 컴포넌트 구성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,21 @@
+'use client'
+
+import HeaderLogo from '@/assets/header-logo.svg'
+import DefaultProfile from '@/assets/default-profile.svg'
+import SvgButton from '@/components/SvgButton'
+import { useRouter } from 'next/navigation'
+
 export default function Header() {
-  return <nav className="fixed top-0 h-14 w-full max-w-150 bg-gray-50">Header</nav>
+  const router = useRouter()
+  return (
+    <nav className="fixed top-0 h-14 w-full max-w-150 bg-white p-4">
+      <div className="flex h-full w-full justify-between">
+        <SvgButton icon={<HeaderLogo />} onClick={() => router.push('/')} />
+        <div className="flex gap-3">
+          {/* TODO: 2차 알림 버튼 추가 */}
+          <SvgButton icon={<DefaultProfile />} onClick={() => router.push('/mypage')} />
+        </div>
+      </div>
+    </nav>
+  )
 }
