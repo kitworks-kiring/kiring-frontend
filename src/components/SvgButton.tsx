43ed0cd5b95b0320cabb3f4d2e07c2cd744a69d9
hyein0112 <s21046@gsm.hs.kr>
2025-05-20 18:26:00 +0900

    feat: SvgButton 공통 컴포넌트 생성

---
+++ src/components/SvgButton.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+interface SvgButtonProps {
+  icon: React.ReactNode
+  onClick: () => void
+  width?: number
+  height?: number
+  bottomTitle?: React.ReactNode
+}
+
+export default function SvgButton({
+  icon: Icon,
+  onClick,
+  width,
+  height,
+  bottomTitle,
+}: SvgButtonProps) {
+  return (
+    <button
+      className={`w-[${width}px] h-[${height}px] flex flex-col items-center justify-center`}
+      type="button"
+      onClick={onClick}
+    >
+      {Icon}
+      {bottomTitle && bottomTitle}
+    </button>
+  )
+}
