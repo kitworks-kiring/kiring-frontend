9f2df502ee4934d09dc1951a92b98add3ae8689b
hyein0112 <bhi12134@gmail.com>
2025-05-28 18:19:03 +0900

    feat: 레이아웃 수정

---
+++ src/app/(main)/components/section-header.tsx
@@ -0,0 +1,19 @@
+import ArrowRight from '@/assets/arrow-right.svg'
+
+interface SectionHeaderProps {
+  time?: string
+  title: string
+  onClick: () => void
+}
+
+export default function SectionHeader({ time, title, onClick }: SectionHeaderProps) {
+  return (
+    <div className="flex items-center justify-between px-4 pt-6 pb-5">
+      <div className="gap flex gap-2">
+        {time && <span className="body2-sb text-purple-200">{time}</span>}
+        <span className="head5 text-black">{title}</span>
+      </div>
+      <ArrowRight onClick={onClick} />
+    </div>
+  )
+}
