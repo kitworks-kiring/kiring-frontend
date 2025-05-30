53bd2f0d5e39c92a16e14fe48b42b9c848944a60
hyein0112 <bhi12134@gmail.com>
2025-05-30 11:00:04 +0900

    feat: 헤더 뒤로가기 버튼 분기처리

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import HeaderLogo from '@/assets/header-logo.svg'
+import ArrowHeader from '@/assets/arrow-header.svg'
 import DefaultProfile from '@/assets/default-profile.svg'
 import SvgButton from '@/components/ui/SvgButton'
 import { NAV_BUTTONS } from '@/components/layout/Navigation/constants'
 import { useRouter, usePathname } from 'next/navigation'
 
-export default function Header() {
+export default function Header({ isBackButton }: { isBackButton?: boolean }) {
   const router = useRouter()
   const pathname = usePathname()
 
@@ -17,15 +18,20 @@ export default function Header() {
   return (
     <nav aria-label="헤더 네비게이션" className="fixed top-0 h-14 w-full max-w-110 bg-white p-4">
       <div className="flex h-full w-full justify-between">
-        {matchedNavItem?.endpoint === '/' ? (
-          <SvgButton
-            ariaLabel="홈으로 이동"
-            icon={<HeaderLogo />}
-            onClick={() => router.push('/')}
-          />
-        ) : (
-          <p className="font-body2-sb">{matchedNavItem?.title}</p>
-        )}
+        <div className="flex items-center gap-4">
+          {isBackButton && (
+            <SvgButton ariaLabel="뒤로가기" icon={<ArrowHeader />} onClick={() => router.back()} />
+          )}
+          {matchedNavItem?.endpoint === '/' ? (
+            <SvgButton
+              ariaLabel="홈으로 이동"
+              icon={<HeaderLogo />}
+              onClick={() => router.push('/')}
+            />
+          ) : (
+            <p className="font-body2-sb">{matchedNavItem?.title}</p>
+          )}
+        </div>
         <div className="flex gap-3">
           {/* TODO: 2차 알림 버튼 추가 */}
           <SvgButton
