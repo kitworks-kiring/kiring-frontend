80f5139e3f42494b5246e3691c44ed913f77ed2b
Hailey Baek <81304598+hyein0112@users.noreply.github.com>
2025-06-02 19:19:33 +0900

    Update src/app/(full-layout)/transit/page.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/app/(full-layout)/transit/page.tsx
+++ src/app/(full-layout)/transit/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react'
 import LoadingSpinner from '@/components/ui/LoadingSpinner'
 import TransitContents from '@/app/(full-layout)/transit/components/TransitContents'
 
-export default async function Transit() {
+export default function Transit() {
   return (
     <Suspense fallback={<LoadingSpinner />}>
       <TransitContents />
