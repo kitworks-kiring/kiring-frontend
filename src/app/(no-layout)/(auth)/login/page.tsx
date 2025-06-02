abb8682d5960436e2531c86824bd2e59b4d4ef08
ydw1996 <didekdnlt1996@gmail.com>
2025-06-02 15:48:15 +0900

    fix: : 로그인 export 명 수정

--- src/app/(no-layout)/(auth)/login/page.tsx
+++ src/app/(no-layout)/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react'
 import LoadingSpinner from '@/components/ui/LoadingSpinner'
 import LoginPage from '@/app/(no-layout)/(auth)/login/components/LoginPage'
 
-export default function Place() {
+export default function Login() {
   return (
     <Suspense fallback={<LoadingSpinner />}>
       <LoginPage />
