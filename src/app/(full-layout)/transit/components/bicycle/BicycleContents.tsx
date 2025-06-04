35a048f02655014b5b51a5bf695a64d4ce78e1fe
Hailey Baek <81304598+hyein0112@users.noreply.github.com>
2025-06-04 22:59:26 +0900

    feat: 따릉이 새로고침 버튼 추가 및 로딩상태 컴포넌트 추가 #47

--- src/app/(full-layout)/transit/components/bicycle/BicycleContents.tsx
+++ src/app/(full-layout)/transit/components/bicycle/BicycleContents.tsx
@@ -1,21 +1,28 @@
 'use client'
+
 import { useEffect, useState } from 'react'
-import { DdarungiStation } from '@/app/(full-layout)/transit/types/bicycleType'
 import dayjs from '@/lib/dayjs'
+import IcoRefresh from '@/assets/ico-refresh.svg'
+import LoadingSpinner from '@/components/ui/LoadingSpinner'
+import { DdarungiStation } from '@/app/(full-layout)/transit/types/bicycleType'
 
 export default function BicycleContents() {
   const [stations, setStations] = useState<DdarungiStation[]>([])
-  const [loading, setLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
   const [createdAt, setCreatedAt] = useState(0)
   async function fetchStations() {
+    setIsLoading(true)
     try {
       const res = await fetch('/api/bicycle')
       const { stations, createdAt } = await res.json()
       setStations(stations ?? [])
       setCreatedAt(createdAt)
-      setLoading(false)
     } catch (error) {
       console.error(error)
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false)
+      }, 300)
     }
   }
 
@@ -23,23 +30,33 @@ export default function BicycleContents() {
     fetchStations()
   }, [])
 
-  if (loading) return <div>로딩중...</div>
   return (
     <>
-      <div className="flex items-center justify-end border-b-2 border-gray-50 px-4 py-3">
-        <div className="body4 text-gray-600">{dayjs(createdAt).format('A h:mm:ss')}</div>
-      </div>
-      <div className="mt-7 grid gap-4 px-4">
-        {stations?.map((station) => (
-          <div key={station.stationId} className="rounded-xl border bg-white p-4">
-            <div className="font-bold">{station.stationName}</div>
-            <div>총 거치대: {station.rackTotCnt}</div>
-            <div>
-              남은 자전거:{' '}
-              <span className="font-bold text-green-600">{station.parkingBikeTotCnt}</span>
-            </div>
+      <div className="full-width fixed top-23 z-10 flex min-h-10 items-center justify-end border-b-2 border-gray-50 bg-white px-4 py-3">
+        <div className="flex items-center gap-2">
+          <div className="body4 text-gray-800">
+            {createdAt ? dayjs(createdAt).format('A h:mm:ss') : ''}
           </div>
-        ))}
+          <button onClick={fetchStations} disabled={isLoading}>
+            <IcoRefresh />
+          </button>
+        </div>
+      </div>
+
+      <div className="mt-24 flex flex-col gap-4 px-4">
+        {isLoading ? (
+          <div className="flex-row-center absolute top-0 left-0 h-full w-full">
+            <LoadingSpinner />
+          </div>
+        ) : (
+          stations?.map((station) => (
+            <div key={station.stationId} className="rounded-xl border bg-white p-4">
+              <div className="font-bold">{station.stationName}</div>
+              <div>총 거치대: {station.rackTotCnt}</div>
+              <div>남은 자전거: {station.parkingBikeTotCnt}</div>
+            </div>
+          ))
+        )}
       </div>
     </>
   )
