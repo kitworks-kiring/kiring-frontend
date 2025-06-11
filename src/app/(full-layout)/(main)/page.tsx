8a840b6fa74e8eb182f318f57b6e5782edaf17db
ydw1996 <didekdnlt1996@gmail.com>
2025-06-11 23:59:44 +0900

    feat: 카카오 비즈니스 심사 대응을 위한 사업자 정보 추가 (#58)

--- src/app/(full-layout)/(main)/page.tsx
+++ src/app/(full-layout)/(main)/page.tsx
@@ -2,7 +2,7 @@ import IntroSection from '@/app/(full-layout)/(main)/components/intro-section'
 import RecommendSection from '@/app/(full-layout)/(main)/components/recommend-section/recommend-section'
 import WeeklyScheduleSection from '@/app/(full-layout)/(main)/components/weekly-schedule-section'
 import MemberSection from '@/app/(full-layout)/(main)/components/member-section'
-
+import Footer from '@/app/(full-layout)/(main)/components/footer'
 export default function Main() {
   return (
     <div className="mx-auto flex flex-col items-center gap-3 bg-gray-50">
@@ -10,6 +10,7 @@ export default function Main() {
       <RecommendSection />
       <WeeklyScheduleSection />
       <MemberSection />
+      <Footer />
     </div>
   )
 }
