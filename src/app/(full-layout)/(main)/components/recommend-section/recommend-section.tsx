0b02632f1e570d8d0b250d34fc0c0e08fffd4424
hyein0112 <bhi12134@gmail.com>
2025-06-12 19:14:55 +0900

    feat: 메인페이지 배경 작업 #68

--- src/app/(full-layout)/(main)/components/recommend-section/recommend-section.tsx
+++ src/app/(full-layout)/(main)/components/recommend-section/recommend-section.tsx
@@ -5,7 +5,7 @@ import PlaceCard from '@/app/(full-layout)/(main)/components/recommend-section/p
 
 export default function RecommendSection() {
   return (
-    <section className="flex w-full flex-col bg-white pb-6">
+    <section className="z-1 mt-[-26px] flex w-full flex-col rounded-t-2xl bg-white pb-6">
       <SectionHeader
         time="Lunch"
         title="오늘 점심은 여기 어때요?"
