e161da5d2607310f874eead77b3bc762b93acf3b
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2025-06-17 20:24:20 +0900

    refactor: 404, error 하나의 status 컴포넌트로 통합 (#80)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,10 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import NotFoundImage from '@/assets/status/not-found.png'
+import Status from '@/components/status/Status'
 
 export default function NotFound() {
   return (
     <main className="default-layout flex flex-col justify-around py-10">
-      <div>
-        <Image src={NotFoundImage} alt="404 일러스트" width={440} height={300} />
-        <p className="head3 text-center">
-          찾고 계신{' '}
-          <span className="text-purple-500">
-            페이지가
-            <br />
-            하늘 어딘가로{' '}
-          </span>
-          날아가버렸어요
-        </p>
-      </div>
+      <Status statusCode="not-found" />
       <Link
         href="/"
         className="body2 left-4 mx-4 rounded-xl bg-purple-500 py-5 text-center text-white"
---
+++ src/components/status/Status/index.tsx
@@ -0,0 +1,45 @@
+import Cloud from '@/assets/status/cloud.svg'
+import SmallPlane from '@/assets/status/small-plane.svg'
+import Status404 from '@/assets/status/status-404.svg'
+import Status500 from '@/assets/status/status-500.svg'
+
+interface StatusProps {
+  statusCode: 'not-found' | 'error'
+}
+
+export default function Status({ statusCode }: StatusProps) {
+  const statusStyle = 'position-centered-x position-centered-y absolute z-1'
+  const textStyle = 'head3 text-center leading-8 -mt-5'
+
+  return (
+    <section>
+      <div className="border-gray-10 relative h-100">
+        <Cloud className="absolute -left-20 z-0" />
+        {statusCode === 'not-found' ? (
+          <Status404 className={statusStyle} />
+        ) : (
+          <Status500 className={statusStyle} />
+        )}
+        <SmallPlane className="absolute top-10 right-5 z-1" />
+        <Cloud className="absolute -right-20 bottom-5 z-2" />
+      </div>
+      {statusCode === 'not-found' ? (
+        <p className={textStyle}>
+          찾고 계신{' '}
+          <span className="text-purple-500">
+            페이지가
+            <br />
+            하늘 어딘가로{' '}
+          </span>
+          날아가버렸어요
+        </p>
+      ) : (
+        <p className={textStyle}>
+          구름 속에서 <span className="text-purple-500">길을 잃었어요</span>
+          <br />
+          잠시 후 다시 시도해주세요
+        </p>
+      )}
+    </section>
+  )
+}
