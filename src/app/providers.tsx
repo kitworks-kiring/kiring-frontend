03ec0668dd40ae466582795bd4bc86904825f387
ydw1996 <didekdnlt1996@gmail.com>
2025-06-17 21:34:06 +0900

    feat: 미들웨어에서 access/refresh 토큰 유효성 검사 임시 로직 추가
    - /member/me API로 accessToken 유효성 검사
    - 실패 시 shouldRemoveCookies 조건으로 쿠키 삭제
    - 보호 페이지 접근 제어 및 리디렉트 반영

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,14 +4,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode, useEffect, useState } from 'react'
 import { SessionProvider } from 'next-auth/react'
 import { useAuthStore } from '@/stores/login'
+import { usePathname } from 'next/navigation'
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient())
-
+  const pathname = usePathname()
+  const { isLogin } = useAuthStore()
   useEffect(() => {
     // 로그인 상태 체크
     useAuthStore.getState().checkToken()
-  }, [])
+    console.log('🚀 ~ Providers ~ pathname:', pathname)
+    console.log('🚀 ~ Providers ~ pathname:', isLogin)
+  }, [pathname, isLogin])
 
   return (
     <SessionProvider>
