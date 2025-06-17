827c17f7c62be75c6d4c6454024e859c56882ae5
hyein0112 <bhi12134@gmail.com>
2025-06-17 11:18:28 +0900

    feat: 커퓨니티 페이지 로그인 했을때만 접근되게 변경 #78

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,5 +24,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/login', '/login/callback', '/mypage/:path*'],
+  matcher: ['/login', '/login/callback', '/mypage/:path*', '/community/:path*'],
 }
