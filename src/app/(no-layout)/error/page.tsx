ef8d744f423f453e4aefba95d014264280791d42
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2025-06-18 14:23:08 +0900

    refactor: 글로벌 Error 객체와의 네이밍 충돌 방지를 위한 함수명 변경 (#80)
    - 전역 Error 객체와의 혼동을 방지하기 위해 컴포넌트명을 Error에서 ErrorPage로 변경

--- src/app/(no-layout)/error/page.tsx
+++ src/app/(no-layout)/error/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Status from '@/components/status/Status'
 
-export default function Error() {
+export default function ErrorPage() {
   return (
     <div className="flex h-full flex-col justify-around py-10">
       <Status statusCode="error" />
