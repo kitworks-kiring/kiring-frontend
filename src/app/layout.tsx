30fa12207e45255bd7870bf8f4a39fb4208bb00a
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2025-06-27 02:11:27 +0900

    chore: 뷰저블 애널리틱스 스크립트 연동 (#98)
    - 사용자 행동 분석을 위한 뷰저블 트래킹 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import Script from 'next/script'
 import { Providers } from './providers'
+import { BeusableScript } from './analytics'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -28,17 +29,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko" className="h-dvh">
       <head>
+        {/* pwa */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#ffffff" />
         <link rel="icon" href="/icons/icon-192x192.png" />
-
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        {/* font */}
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
         />
+        {/* kakaomap */}
         <Script
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false&libraries=services,clusterer`}
           strategy="beforeInteractive"
@@ -48,6 +51,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Providers>
           <div className="full-width mx-auto flex h-full flex-col bg-white">{children}</div>
         </Providers>
+        {/* analytics: </body> 태그 바로 앞에 위치해야 함 */}
+        <BeusableScript />
       </body>
     </html>
   )
